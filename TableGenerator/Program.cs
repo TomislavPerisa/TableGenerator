99a6798c716f8e6306fad30ec79ea698917cc9d6
Tomislav Periša <tomislav.pingi.perisa@gmail.com>
2024-02-22 00:51:11 +0100

    Added code for testing

--- TableGenerator/Program.cs
+++ TableGenerator/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using TableGenerator;
+using TableGeneratorLibrary;
+
+var car = new Car
+{
+    Manufacturer = "Audi",
+    MaxSpeed = 100,
+    Model = "R8",
+    Weight = 1000.5f,
+};
+
+var table = new Table<Car>(new Car[] {car});
+foreach(var property in table.GetHeaders())
+{
+    Console.WriteLine(property);
+}
+Console.WriteLine();
+Console.WriteLine(table.Show());
