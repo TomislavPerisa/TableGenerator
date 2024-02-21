7787e5196646f124a49f9c7be5fe093791c58554
Tomislav Periša <tomislav.pingi.perisa@gmail.com>
2024-02-22 00:48:18 +0100

    Added test class

---
+++ TableGenerator/Car.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TableGenerator
+{
+    public class Car
+    {
+        [DisplayName("Identification")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Manufacturer { get; set; }
+        public string? Model { get; set; }
+        public int MaxSpeed { get; set; }
+        public float Weight { get; set; }
+    }
+}
